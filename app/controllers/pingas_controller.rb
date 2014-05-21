c817ab3a2f1da0ab9bdc4318c4517f09f9a66116
Daniel Armstrong <danielarmstrong@Daniels-MacBook-Pro.local>
2014-05-21 18:59:19 -0500

    added map view and js

--- app/controllers/pingas_controller.rb
+++ app/controllers/pingas_controller.rb
@@ -1,5 +1,10 @@
 class PingasController < ApplicationController
   def index
+    # @users = User.all
+    # @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+    #   marker.lat user.latitude
+    #   marker.lng user.longitude
+    # end
   end
 
   def show
