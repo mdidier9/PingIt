46b702ffa666f90e1ae5fe8d2ec72ecd91c35977
Matt Didier <mdidier9@gmail.com>
2014-05-21 17:22:05 -0500

    updated to 4.0.0

---
+++ ping/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ping::Application.config.secret_key_base = 'c94671f03d412f37efaa533ffb071d06301c17888f9ff80034dcaab0bd266781d559d12827b46beb0710142ee8dd81028885c0a9a44eaf296e16c91a5d169877'
