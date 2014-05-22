ce6c8a9db7e6beee03e3ad8e8dbcab6bb77e05f9
Daniel Armstrong <danielarmstrong@Daniels-MacBook-Pro.local>
2014-05-21 19:59:03 -0500

    Added the seed file with users and pingas

--- app/controllers/pingas_controller.rb
+++ app/controllers/pingas_controller.rb
@@ -1,10 +1,5 @@
 class PingasController < ApplicationController
   def index
-    # @users = User.all
-    # @hash = Gmaps4rails.build_markers(@users) do |user, marker|
-    #   marker.lat user.latitude
-    #   marker.lng user.longitude
-    # end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Needs status
+User.create!()
+User.create!()
+User.create!()
+
+Pinga.create!(title: "Food Truck", description: "There is food.", status: "started", start_time: Time.now, end_time: Time.now, address: "351 W Hubbard St, Chicago, IL 60654", location: Location.create, creator_id: 1)
+
+Pinga.create!(title: "Sport town", description: "Play the game", status: "started", start_time: Time.now, end_time: Time.now, address: "630 N Kingsbury St Chicago, IL 60654", location: Location.create, creator_id: 2)
+
+Pinga.create!(title: "Drinkos", description: "We're at a bar", status: "started", start_time: Time.now, end_time: Time.now, address: "155 W Kinzie St, Chicago, IL 60654", location: Location.create, creator_id: 3)
