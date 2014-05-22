48fcef74fc77837ab922b31d3a6dc756dc68c577
Tam T Dang <teedang19@gmail.com>
2014-05-22 08:29:11 -0500

    updated omniauth.rb with comment should not show

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV["FACEBOOK_KEY"]="764749046879221", ENV["FACEBOOK_SECRET"]='deae0216c677f64553f7a7aad2ea3686'
 end
+
+# does this change show?
