424aa4f59eb656046808b7f34b28a0f1916e4516
Matt Didier <mdidier9@gmail.com>
2014-05-21 19:58:19 -0500

    updated models and migrations (DROP AND REMIGRATE)

--- db/migrate/20140521231751_create_users.rb
+++ db/migrate/20140521231751_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
     	t.string	 :oauth_token
     	t.datetime :oauth_expires_at
-    	t.integer  :location_id
+    	t.float 	 :latitude
+    	t.float	 	 :longitude
       t.timestamps
     end
   end
