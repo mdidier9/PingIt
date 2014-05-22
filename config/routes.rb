ca4b2189dd71a75e7ea8c50471742effc5d7e971
Tam T Dang <teedang19@gmail.com>
2014-05-21 20:54:46 -0500

    edit routes for FB login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Ping::Application.routes.draw do
   resources :pingas
   resources :sessions
   resources :users
+
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 end
