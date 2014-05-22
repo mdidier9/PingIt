a99593cff13265ebfa40c8212c81d7f4e32e92a5
Tam T Dang <teedang19@gmail.com>
2014-05-21 21:50:14 -0500

    remove unnecessary sessions#index method, adjust application.html.erb with correct signout route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,5 @@
 class SessionsController < ActionController::Base
 
-  # def index
-  #   @user = User.find_by_id(session[:user_id])
-  # end
-
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
