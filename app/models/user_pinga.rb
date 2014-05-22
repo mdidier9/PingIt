5002d148a3420469c18cfa2ff37a6024537c0507
Tam T Dang <teedang19@gmail.com>
2014-05-22 09:29:40 -0500

    add validations to user_pinga model and corresponding tests

--- app/models/user_pinga.rb
+++ app/models/user_pinga.rb
@@ -1,4 +1,9 @@
 class UserPinga < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :pinga
+
+  validates :rsvp_status, inclusion: { in: ["attending", "not attending", nil] }
+  validates :attend_status, inclusion: { in: ["attended", "did not attend", nil] }
+  validates :user_id, presence: true
+  validates :pinga_id, presence: true
 end
