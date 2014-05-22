9c2e682cca5ee4bcb87cd4ea16291f587710adc5
Tam T Dang <teedang19@gmail.com>
2014-05-22 10:42:26 -0500

    first tests and validations on pinga models

--- app/models/pinga.rb
+++ app/models/pinga.rb
@@ -2,4 +2,7 @@ class Pinga < ActiveRecord::Base
 	belongs_to :creator, class_name: "User"
 	has_many :user_pingas
 	has_many :users, through: :user_pingas
+
+  validates :status, inclusion: { in: ["pending", "active", "inactive"] }
+  validates :title, :description, :address, presence: true
 end
