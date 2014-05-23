95a2a6d1c8e144a9de16955e3ec0cf7c9224e902
Tam T Dang <teedang19@gmail.com>
2014-05-23 09:16:46 -0500

    Pinga map relocates, YAY

--- app/assets/javascripts/autofill_address.js
+++ app/assets/javascripts/autofill_address.js
@@ -24,17 +24,30 @@ $(document).on("page:load", function() {
 $(document).ready(function() {
 
   autocomplete = new google.maps.places.Autocomplete(
-      (document.getElementById('autocomplete')),
-      { types: ['geocode'] });
+    (document.getElementById('autocomplete')),
+    { types: ['geocode'] });
 
-  $('#autocomplete').on("change", function() {
-    console.log("change here");
-  })
+  var map = new google.maps.Map(document.getElementById('create-pinga-map'));
+
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    console.log(place);
+    map.setCenter(place.geometry.location);
+    map.setZoom(17);
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+  });
 
   $('form').on("submit", function(event) {
     event.preventDefault();
 
-    var data = {
+    var data = { // form redirection
       title: $($('input#pinga_title')[0]).val(),
       description: $($('#pinga_description')[0]).val(),
       address: $('#autocomplete').val(),
