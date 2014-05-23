1ad3daaf06c82500df7b262fba3c76a12a7f1c89
Tam T Dang <teedang19@gmail.com>
2014-05-22 22:57:59 -0500

    Ajex response setup for new pingas

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ gem 'rails', '4.0.0'
 
 # Use postgresql as the database for Active Record
 gem 'pg'
-
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- app/assets/javascripts/autofill_address.js
+++ app/assets/javascripts/autofill_address.js
@@ -6,6 +6,9 @@ $(document).on("page:load", function() {
       (document.getElementById('autocomplete')),
       { types: ['geocode'] });
 
+  $('#autocomplete').on("change", function() {
+    console.log("change here");
+  })
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -24,6 +27,27 @@ $(document).ready(function() {
       (document.getElementById('autocomplete')),
       { types: ['geocode'] });
 
+  $('#autocomplete').on("change", function() {
+    console.log("change here");
+  })
+
+  $('form').on("submit", function(event) {
+    event.preventDefault();
+
+    var data = {
+      title: $($('input#pinga_title')[0]).val(),
+      description: $($('#pinga_description')[0]).val(),
+      address: $('#autocomplete').val(),
+      start_time: $($('#pinga_start_time')[0]).val(),
+      end_time: $($('#pinga_end_time')[0]).val()
+    };
+
+    $.post('/pingas', data, function(response) {
+      console.log("response here!");
+      console.log("response");
+    });
+
+  });
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
