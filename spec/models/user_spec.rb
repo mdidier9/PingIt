b84ea9d2120a37fc1dcb2b3a82df8ceefa4600f2
Matt Didier <mdidier9@gmail.com>
2014-05-24 14:57:58 -0500

    updated user_spec functional

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 describe User do
-  before :each do
-    user = User.create!(listening_radius: 0.3, ip_address: "74.122.9.196")
-    grey = Pinga.create!(title: "Sport town", description: "Play the game", status: "pending", start_time: Time.now, end_time: Time.now, address: "630 N Kingsbury St Chicago, IL 60654", creator_id: 2)
-    active = Pinga.create!(title: "Food Truck", description: "There is food.", status: "active", start_time: Time.now, end_time: Time.now, address: "351 W Hubbard St, Chicago, IL 60654", creator_id: 1)
-    pending = Pinga.create!(title: "Drinkos", description: "We're at a bar", status: "pending", start_time: Time.now, end_time: Time.now, address: "155 W Kinzie St, Chicago, IL 60654", creator_id: 3)
-  end
+    let!(:user) {User.create!(listening_radius: 0.3, ip_address: "74.122.9.196")}
+    let!(:grey) {Pinga.create!(title: "Sport town", description: "Play the game", status: "pending", start_time: Time.now, end_time: Time.now, address: "630 N Kingsbury St Chicago, IL 60654", creator_id: 3)}
+    let!(:active) {Pinga.create!(title: "Food Truck", description: "There is food.", status: "active", start_time: Time.now, end_time: Time.now, address: "351 W Hubbard St, Chicago, IL 60654", creator_id: 2)}
+    let!(:pending) {Pinga.create!(title: "Drinkos", description: "We're at a bar", status: "pending", start_time: Time.now, end_time: Time.now, address: "155 W Kinzie St, Chicago, IL 60654", creator_id: 1)}
 
   context "user associations" do
     it { should have_many(:created_pingas) }
@@ -16,42 +14,23 @@ describe User do
 
   context "active_pingas_in_listening_radius" do
     it "should return active pingas in listening radius" do
-      expect (user.active_pingas_in_listening_radius).to eq([active])
+      user.geocode
+      expect(user.active_pingas_in_listening_radius).to eq([active])
     end
   end
 
   context "pending_pingas_in_listening_radius" do
     it "should return pending pingas in listening radius" do
-      expect (user.pending_pingas_in_listening_radius).to eq([pending])
+      user.geocode
+      expect(user.pending_pingas_in_listening_radius).to eq([pending])
     end
   end
 
   context "pingas_outside_listening_radius" do
     it "should return pingas outside of user listening radius" do
-      expect (user.pingas_outside_listening_radius).to eq([grey])
+      user.geocode
+      expect(user.pingas_outside_listening_radius).to eq([grey])
     end
   end
-
-  context "active_pinga_markers" do
-    it "should return Gmaps4rails object" do
-      expect(user.active_pinga_markers).to be_a_kind_of(Gmaps4rails)
-    end
-  end
-
-  context "pending_pinga_markers" do
-    it "should return Gmaps4rails object" do
-      expect(user.pending_pinga_markers).to be_a_kind_of(Gmaps4rails)
-    end
-  end
-
-  context "grey_pinga_markers" do
-    it "should return Gmaps4rails object" do
-      expect(user.grey_pinga_markers).to be_a_kind_of(Gmaps4rails)
-    end
-  end
-
-
-
-
 
 end
