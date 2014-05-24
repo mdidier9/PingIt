73d1770ec08e0248b9e499c8fc74ff82a712615f
Rohan Mathur <rmathur101@gmail.com>
2014-05-23 20:55:13 -0500

    accessing params coming in from phone

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -2,6 +2,7 @@ class PhonesController < ApplicationController
 
 	def recieve
 		puts "THIS IS SOMETHING IN THE PHONESCONTROLLER **************************************************************"
+		p params[:data]
 
 	end
 
