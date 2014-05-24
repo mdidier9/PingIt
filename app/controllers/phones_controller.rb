eda19e1f77ec4b2247a105247e7341ab4f2cf1be
Rohan Mathur <rmathur101@gmail.com>
2014-05-23 22:11:26 -0500

    successfully getting into recieve action in phone controller; able to print out params; need to get rest of data needed from phone; need to write methods in webapp to get pinga migration data using phone data; note that require_login has been commented out to allow phone to proper http get request to /phone

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -4,6 +4,31 @@ class PhonesController < ApplicationController
 		p "THIS IS SOMETHING IN THE PHONESCONTROLLER ********************"
 		p params[:data]
 
+
+		#this is currently the format of the new_ping hash that we are getting from the phone
+		#----------------------------------------------------------------------------------------
+		# {"title"=>"", "description"=>"", "category"=>"Social", "start_time"=>"6:00 PM", "end_time"=>"8:00 PM", "address"=>""}  
+
+
+
+		#this is currently the format of the new_ping properties that is defined in our migrations
+		#----------------------------------------------------------------------------------------
+		
+		#CHECK 	t.string 		:title 
+    #CHECK 	t.string 		:description
+    #NO (determine in web app --- method) 	t.string 		:status            
+    #CHECK 	t.datetime 	:start_time
+    #CHECK 	t.datetime 	:end_time
+    #CHECK 	t.string		:address
+    #NO (get from the phone using bubblewraps's location obj)   t.float     :latitude
+    #NO (get from the phone using bubblewraps's location obj)   t.float     :longitude
+    #NO (need to somehow get the user information from the phone) 	t.integer 	:creator_id #user_id
+    #CHECK   t.timestamps (this will be created when it is entered in the database)
+
+
+
+
+
 	end
 
 end
