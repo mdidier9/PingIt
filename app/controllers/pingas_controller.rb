9cadaed89d811abaf4a03ffcd194a789e398f470
Daniel Armstrong <danielarmstrong@Daniels-MacBook-Pro.local>
2014-05-24 14:44:08 -0500

    fixed bugs

--- app/controllers/pingas_controller.rb
+++ app/controllers/pingas_controller.rb
@@ -5,7 +5,33 @@ class PingasController < ApplicationController
     @user.geocode
     @user.update_user_pingas
     @user_marker = @user.marker
-    @pinga_markers = @user.pinga_markers
+    active_markers = Gmaps4rails.build_markers(@user.active_pingas_in_listening_radius) do |pinga, marker|
+      marker.lat pinga.latitude
+      marker.lng pinga.longitude
+      marker.infowindow render_to_string(:partial => "/shared/infowindow", :locals => { pinga: pinga })
+      marker.picture({ "url" => "assets/active.png",
+                       "width" => 20,
+                       "height" => 34})
+    end
+
+    pending_markers = Gmaps4rails.build_markers(@user.pending_pingas_in_listening_radius) do |pinga, marker|
+      marker.lat pinga.latitude
+      marker.lng pinga.longitude
+      marker.infowindow render_to_string(:partial => "/shared/infowindow", :locals => { pinga: pinga })
+      marker.picture({"url" => "assets/pending.png",
+                      "width" => 20,
+                      "height" => 34})
+    end
+
+    grey_markers = Gmaps4rails.build_markers(@user.pingas_outside_listening_radius) do |pinga, marker|
+      marker.lat pinga.latitude
+      marker.lng pinga.longitude
+      marker.infowindow render_to_string(:partial => "/shared/infowindow", :locals => { pinga: pinga })
+      marker.picture({  "url" => "assets/grey.png",
+                        "width" => 20,
+                        "height" => 34})
+    end
+    @pinga_markers = active_markers + pending_markers + grey_markers
     @pingas_by_received_time = @user.pingas_ordered_by_received_in_listening_radius
     @pingas_by_distance = @user.pingas_ordered_by_distance_in_listening_radius
     @pingas_by_start_time = @user.pingas_ordered_by_start_time_in_listening_radius
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,45 +28,6 @@ class User < ActiveRecord::Base
     end
   end
 
-  def pinga_markers
-    self.active_pinga_markers + self.pending_pinga_markers + self.grey_pinga_markers
-  end
-
-  def active_pinga_markers
-    Gmaps4rails.build_markers(self.active_pingas_in_listening_radius) do |pinga, marker|
-      marker.lat pinga.latitude
-      marker.lng pinga.longitude
-      marker.infowindow(pinga.title)
-      marker.picture({ "url" => "assets/active.png",
-                       "width" => 20,
-                       "height" => 34})
-    end
-  end
-
-  def pending_pinga_markers
-    Gmaps4rails.build_markers(self.pending_pingas_in_listening_radius) do |pinga, marker|
-      marker.lat pinga.latitude
-      marker.lng pinga.longitude
-      marker.infowindow(pinga.title)
-      marker.picture({"url" => "assets/pending.png",
-                      "width" => 20,
-                      "height" => 34})
-    end
-  end
-
-  def grey_pinga_markers
-    Gmaps4rails.build_markers(self.pingas_outside_listening_radius) do |pinga, marker|
-      marker.lat pinga.latitude
-      marker.lng pinga.longitude
-      marker.infowindow(pinga.title)
-      marker.picture({  "url" => "assets/grey.png",
-                        "width" => 20,
-                        "height" => 34})
-    end
-  end
-
-
-
   def active_pingas_in_listening_radius
     Pinga.near(self, self.listening_radius).where(status: "active")
   end
