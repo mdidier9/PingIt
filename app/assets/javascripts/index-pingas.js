3b0ac0006f5e6ec8ff91df88c7a290973cb36a1f
Tam T Dang <teedang19@gmail.com>
2014-05-25 14:44:01 -0500

    nothing

--- app/assets/javascripts/index-pingas.js
+++ app/assets/javascripts/index-pingas.js
@@ -1,33 +1,33 @@
-function initialize() {
-    var mapOptions = {
-        zoom: 4,
-        center: new google.maps.LatLng(-25.363882, 131.044922)
-    };
+// function initialize() {
+//     var mapOptions = {
+//         zoom: 4,
+//         center: new google.maps.LatLng(-25.363882, 131.044922)
+//     };
 
-    var map = new google.maps.Map(document.getElementById('map'),
-        mapOptions);
+//     var map = new google.maps.Map(document.getElementById('map'),
+//         mapOptions);
 
-    var marker = new google.maps.Marker({
-        position: map.getCenter(),
-        map: map,
-        title: 'Click to zoom'
-    });
+//     var marker = new google.maps.Marker({
+//         position: map.getCenter(),
+//         map: map,
+//         title: 'Click to zoom'
+//     });
 
-    google.maps.event.addListener(map, 'center_changed', function() {
-        // 3 seconds after the center of the map has changed, pan back to the
-        // marker.
-        window.setTimeout(function() {
-            map.panTo(marker.getPosition());
-        }, 3000);
-    });
+//     google.maps.event.addListener(map, 'center_changed', function() {
+//         // 3 seconds after the center of the map has changed, pan back to the
+//         // marker.
+//         window.setTimeout(function() {
+//             map.panTo(marker.getPosition());
+//         }, 3000);
+//     });
 
-    google.maps.event.addListener(marker, 'click', function() {
-        map.setZoom(8);
-        map.setCenter(marker.getPosition());
-    });
-}
+//     google.maps.event.addListener(marker, 'click', function() {
+//         map.setZoom(8);
+//         map.setCenter(marker.getPosition());
+//     });
+// }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, 'load', initialize);
 
 //map_style = [{featureType:"water",elementType:"geometry",stylers:[{color:"#a2daf2"}]},{featureType:"landscape.man_made",elementType:"geometry",stylers:[{color:"#f7f1df"}]},{featureType:"landscape.natural",elementType:"geometry",stylers:[{color:"#d0e3b4"}]},{featureType:"landscape.natural.terrain",elementType:"geometry",stylers:[{visibility:"off"}]},{featureType:"poi.park",elementType:"geometry",stylers:[{color:"#bde6ab"}]},{featureType:"poi",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"poi.medical",elementType:"geometry",stylers:[{color:"#fbd3da"}]},{featureType:"poi.business",stylers:[{visibility:"off"}]},{featureType:"road",elementType:"geometry.stroke",stylers:[{visibility:"off"}]},{featureType:"road",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"road.highway",elementType:"geometry.fill",stylers:[{color:"#ffe15f"}]},{featureType:"road.highway",elementType:"geometry.stroke",stylers:[{color:"#efd151"}]},{featureType:"road.arterial",elementType:"geometry.fill",stylers:[{color:"#ffffff"}]},{featureType:"road.local",elementType:"geometry.fill",stylers:[{color:"black"}]},{featureType:"transit.station.airport",elementType:"geometry.fill",stylers:[{color:"#cfb2db"}]}]
 //map_options = { maxZoom: 15, styles: map_style };
