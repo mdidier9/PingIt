c6c74e84646a65d49c08ec0e886285eb33973969
Rohan Mathur <rmathur101@gmail.com>
2014-05-25 21:19:56 -0500

    commented out the current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
 
   def require_login
     unless current_user
