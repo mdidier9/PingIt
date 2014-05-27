e4a923b40b2f8c12fe9deb652555af2d45708879
Daniel Armstrong <danielarmstrong@Daniels-MacBook-Pro.local>
2014-05-27 09:42:13 -0500

    Fixed user settings lists from dissppearing

--- app/assets/javascripts/socket.js
+++ app/assets/javascripts/socket.js
@@ -3,3 +3,9 @@ dispatcher = new WebSocketRails('localhost:3000/websocket');
 function submitToSocket(data) {
     dispatcher.trigger('tasks.create', data);
 }
+
+function deleteToSocket(id) {
+    console.log(id);
+    console.log("message from deletetosocket");
+    dispatcher.trigger('tasks.destroy', id)
+}
--- config/events.rb
+++ config/events.rb
@@ -1,5 +1,6 @@
 WebsocketRails::EventMap.describe do
   namespace :tasks do
     subscribe :create, :to => TaskController, :with_method => :create
+    subscribe :destroy, :to => TaskController, :with_method => :destroy
   end
 end
