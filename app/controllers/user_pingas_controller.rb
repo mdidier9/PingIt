bd60a1919b2f37c4a74b13d54f24d75c82ae0744
Tam T Dang <teedang19@gmail.com>
2014-05-27 08:55:20 -0500

    change show to operate on ping_title

---
+++ app/controllers/user_pingas_controller.rb
@@ -0,0 +1,20 @@
+class UserPingasController < ApplicationController
+  def update
+    user_pinga = UserPinga.find(params[:id])
+    if params[:rsvp_status] == "attending"
+      user_pinga.rsvp_status = params[:rsvp_status]
+    else
+      user_pinga.rsvp_status = nil
+    end
+    user_pinga.save
+
+    if user_pinga.rsvp_status == "attending"
+      render json: {attending: true}
+      # respond_to do |format|
+      #   format.js { render json: {attending: true} }
+      # end
+    else
+      render json: {attending: false}
+    end
+  end
+end
