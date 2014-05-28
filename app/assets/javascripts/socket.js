45f37c0d0616288d7cb6f8a9d401cba3b3250362
Daniel Armstrong <danielarmstrong@Daniels-MacBook-Pro.local>
2014-05-28 18:21:26 +0000

    precompile

--- app/assets/javascripts/socket.js
+++ app/assets/javascripts/socket.js
@@ -1,5 +1,6 @@
 //dispatcher = new WebSocketRails('localhost:5000/websocket');
 dispatcher = new WebSocketRails('pinggit.herokuapp.com/websocket');
+console.log(dispatcher);
 channel = dispatcher.subscribe('pingas');
 
 function submitToSocket(data) {
