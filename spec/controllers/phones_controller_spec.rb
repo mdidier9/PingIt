7378a7d839d80a48dcd031fd1ee7e6128d78b6e2
Matt Didier <mdidier9@gmail.com>
2014-05-28 10:48:51 -0500

    updated phones controller spec

--- spec/controllers/phones_controller_spec.rb
+++ spec/controllers/phones_controller_spec.rb
@@ -5,9 +5,10 @@ describe PhonesController do
 
 	context "receive request get events" do
 		it "does something with phone" do
-   		data = {data: {uid: 1, oauth_expires_at: Time.now, oauth_token: 1, name: "name", provider: "facebook" }}
-			get(:recieve_request_get_events, data)
-			response.status.should eq 200
+   		data = {data: {uid: "1", oauth_expires_at: Time.now, oauth_token: 1, name: "name", provider: "facebook", latitude: 41.8876, longitude: -87.6368 }}
+			get(:recieve_request_get_events, data.merge(format: :json))
+			pinga_hash = {"pingas_active_in_radius"=>[], "pingas_pending_in_radius"=>[], "pingas_outside_radius"=>[]}
+			expect(JSON.parse(response.body)).to include(pinga_hash)
 		end
 	end
 
