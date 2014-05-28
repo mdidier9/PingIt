9e33bf80a2458dd655363f51dbf9041e3f24b9d9
Daniel Armstrong <danielarmstrong@Daniels-MacBook-Pro.local>
2014-05-28 15:41:44 +0000

    back to no options

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :driver => :hiredis)
+REDIS = Redis.new(:url => ENV['REDISTOGO_URL'], :driver => :hiredis)
