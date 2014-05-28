6cdad31738c83aec531393e97d859d2dbdd6450d
Tam T Dang <teedang19@gmail.com>
2014-05-28 09:49:02 -0500

    RSVP COUNT updates and persists across all views

--- app/assets/javascripts/rsvp.js
+++ app/assets/javascripts/rsvp.js
@@ -3,6 +3,8 @@ $(function() {
   $(document).on("click", 'div[class^=rsvp]', function(event){
     userPingaId = /\d*$/.exec(this.id)[0];
     var currentPingaDivClass = $(this).attr("class");
+    var pingaId = /\d*$/.exec(currentPingaDivClass)[0];
+    console.log(pingaId);
     var current = $($(this).find(':first-child')).html();
     if (current == "You are not going!") {
       var rsvp = "attending";
@@ -19,11 +21,15 @@ $(function() {
         data: { rsvp_status: rsvp},
         dataType: 'json',
         success: function (data) {
+          var rsvpSpan = $('span[class=rsvp-count-' + pingaId + ']')
+          var currentCount = parseFloat(rsvpSpan.html());
+          // var currentCount = rsvp
           if(data.attending == true) {
             $("."+currentPingaDivClass).html("<p>You are going!</p>");
-            // RSVP count += 1 ON ALL VIEWS
+            rsvpSpan.html(currentCount+1);
           } else {
             $("."+currentPingaDivClass).html("<p>You are not going!</p>");
+            rsvpSpan.html(currentCount-1);
             // RSVP count -= 1 ON ALL VIEWS
           }
         }
