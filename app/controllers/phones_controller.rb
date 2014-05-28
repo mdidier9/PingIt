7b373c49d975ef27cd55048406f8d2118846aa75
Daniel Armstrong <danielarmstrong@Daniels-MacBook-Pro.local>
2014-05-28 19:12:30 -0400

    changed pinga update to pinga new

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -117,7 +117,7 @@ skip_before_filter :require_login, :only => [:recieve_request_get_events, :recie
 		user_pinga.save  
 #------------------------------------------------------------------------------
 		
-    WebsocketRails[:pingas].trigger('update', {id: @pinga.id, status: @pinga.status, category: @pinga.category.title}.to_json)
+    WebsocketRails[:pingas].trigger('new', {id: @pinga.id, status: @pinga.status, category: @pinga.category.title}.to_json)
 		
 		puts "THIS IS THIS THE CREATED EVENT"
 		p @pinga
